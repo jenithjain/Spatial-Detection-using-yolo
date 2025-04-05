8e2121b42850b000a734f8c452c1de4e1171834d
yashpunmiya <yyash7379@gmail.com>
2025-04-06 02:40:17 +0530

    redirection repair

--- staff/urls.py
+++ staff/urls.py
@@ -7,7 +7,8 @@ app_name = 'staff'
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(
         template_name='staff/registration/login.html',
-        redirect_authenticated_user=True
+        redirect_authenticated_user=True,
+        next_page='staff:dashboard'
     ), name='login'),
     path('register/', views.staff_register, name='register'),
     path('dashboard/', views.staff_dashboard, name='dashboard'),
